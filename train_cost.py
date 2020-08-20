92812dcdd99a0e0c911ded6b651632dd483c07da
xjh <414581775@qq.com>
2020-08-20 16:46:49 -0400

    fix bugs

--- train_cost.py
+++ train_cost.py
@@ -95,7 +95,7 @@ def train(nbatches, npred):
             actions = torch.normal(model.stats['a_mean'], model.stats['a_std'], size=actions.size())
         pred, _ = model(inputs[: -1], actions, targets, z_dropout=0)
         if model.opt.output_h and opt.pred_from_h:
-            pred_cost = cost(pred[0].view(opt.batch_size*opt.npred, 1, n_channels, opt.height, opt.width), pred[1].view(opt.batch_size*opt.npred, 1, 4), hidden=pred[2].view(opt.batch_size*opt.npred, 1, -1))
+            pred_cost = cost(pred[0].view(opt.batch_size*opt.npred, 1, n_channels, opt.height, opt.width), pred[1].view(opt.batch_size*opt.npred, 1, 4), hidden=pred[3].view(opt.batch_size*opt.npred, 1, -1))
         else:
             pred_cost = cost(pred[0].view(opt.batch_size*opt.npred, 1, n_channels, opt.height, opt.width), pred[1].view(opt.batch_size*opt.npred, 1, 4))
         pred_images = pred[0]
@@ -139,7 +139,7 @@ def test(nbatches, npred):
         inputs, actions, targets, _, car_sizes = dataloader.get_batch_fm('valid', npred)
         pred, _ = model(inputs[: -1], actions, targets, z_dropout=0)
         if model.opt.output_h and opt.pred_from_h:
-            pred_cost = cost(pred[0].view(opt.batch_size*opt.npred, 1, n_channels, opt.height, opt.width), pred[1].view(opt.batch_size*opt.npred, 1, 4), hidden=pred[2].view(opt.batch_size*opt.npred, 1, -1))
+            pred_cost = cost(pred[0].view(opt.batch_size*opt.npred, 1, n_channels, opt.height, opt.width), pred[1].view(opt.batch_size*opt.npred, 1, 4), hidden=pred[3].view(opt.batch_size*opt.npred, 1, -1))
         else:
             pred_cost = cost(pred[0].view(opt.batch_size * opt.npred, 1, n_channels, opt.height, opt.width),
                              pred[1].view(opt.batch_size * opt.npred, 1, 4))
