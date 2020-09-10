47057366cacff228e658525fcd5606d17e2731c7
xjh <414581775@qq.com>
2020-09-10 18:17:53 -0400

    fix bugs

--- models.py
+++ models.py
@@ -133,9 +133,8 @@ class u_network(nn.Module):
     def forward(self, h):
         h1 = self.encoder(h)
         h2 = self.fc(h1.view(-1, self.hidden_size_input))
-        output_size = h.size()
-        output_size[1] = output_size[1]//3
-        h2 = h2.view(output_size)
+        output_size = h1.size()
+        h2 = h2.view(output_size[0], output_size[1]//2, output_size[2], output_size[3])
         h3 = self.decoder(h2)
         return h3
 
