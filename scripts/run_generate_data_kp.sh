86320fa4b308605fb0ff9804bfacf6b4276ef663
Jiuhong Xiao <jx1190@nyu.edu>
2020-11-09 16:50:46 -0500

    scripts

--- scripts/run_generate_data_kp.sh
+++ scripts/run_generate_data_kp.sh
@@ -1,8 +1,8 @@
 set -k
-for ksize in 7 15; do
-  for position_threshold in 1 5 10 20 50; do
-    for t in 1 2 3; do
-    colored_lane=$ksize"g"$position_threshold"actrajectory.jpg"
+for ksize in 7; do
+  for position_threshold in 1; do
+    for t in 0 1 2; do
+    colored_lane=$ksize"g"$position_threshold"actrajectory.png"
     data_dir=traffic-data/state-action-cost-$ksize-$position_threshold/
     sbatch submit_generate_data_kp.slurm \
             colored_lane=$colored_lane \
